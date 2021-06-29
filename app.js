70654031149fc5b8834e9e672da21988f9c8e2c7
Aritra1296 <aritrivu@gmail.com>
2021-06-29 18:50:15 +0530

    change cors

--- app.js
+++ app.js
@@ -10,12 +10,7 @@ require('dotenv/config')
 //COFIGURE CORS
 app.use(bodyParser.json());
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: ['http://localhost:3000'],
-    credentials : true
-  })
-)
+app.use(cors());
 
 //IMPORT ROUTER
 app.use('/posts', require('./routes/posts'))
